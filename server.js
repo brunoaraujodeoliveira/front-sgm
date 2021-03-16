ede8f3f6ae606b76ca6ac9d7ef353ba20441435e
brunoaraujodeoliveira <brunimaraujodeoliveira@gmail.com>
2021-03-16 02:36:27 -0300

    server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
 );
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 app.listen(server_port || server_host);
