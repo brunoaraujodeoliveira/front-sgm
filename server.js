98ad9e50d624716ddeb10ecd691a41e8cd61bdaa
brunoaraujodeoliveira <brunimaraujodeoliveira@gmail.com>
2021-03-23 17:45:32 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//Install express server
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
@@ -7,12 +6,18 @@ const app = express();
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('/*', function(req,res) { 
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
